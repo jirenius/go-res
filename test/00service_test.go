9a91053d5d91a1732c2ec7a8aae5c40bfb97a681
Samuel Jirénius <samuel@jirenius.com>
2020-03-04 11:15:18 +0100

    GH-69: Added usage example for restest.

--- test/00service_test.go
+++ test/00service_test.go
@@ -10,6 +10,29 @@ import (
 	"github.com/jirenius/go-res/restest"
 )
 
+// TestService is the doc.go usage example
+func TestService(t *testing.T) {
+	// Create service to test
+	s := res.NewService("foo")
+	s.Handle("bar.$id",
+		res.Access(res.AccessGranted),
+		res.GetModel(func(r res.ModelRequest) {
+			r.Model(struct {
+				Message string `json:"msg"`
+			}{r.PathParam("id")})
+		}),
+	)
+
+	// Create test session
+	c := restest.NewSession(t, s)
+	defer c.Close()
+
+	// Test sending get request and validate response
+	c.Get("foo.bar.42").
+		Response().
+		AssertModel(map[string]string{"msg": "42"})
+}
+
 // Test that the service returns the correct protocol version
 func TestServiceProtocolVersion(t *testing.T) {
 	s := res.NewService("test")
